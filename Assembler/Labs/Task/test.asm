c81246e5c3f436d868dd196dd920cb9404cb4747
nikotinko <f.e.a.r_dot@bk.ru>
2016-11-16 20:53:10 +0400

    iml

--- Assembler/Labs/Task/test.asm
+++ Assembler/Labs/Task/test.asm
@@ -6,10 +6,29 @@ model small
 	output db 18
 	db 18 dup('$')
 	number dq 38.0214512
+	numBuffer dq ?
+	pow dw 1
+	fsttmp dw ?
 .code
 start:
 	mov ax, @data
 	mov ds, ax
+	finit
+	;магия
+	fnstcw fsttmp
+  	or     [fsttmp], 0C00h
+  	fldcw  fsttmp
+
+  	mov numBuffer, number
+  	fld numBuffer
+  	@lop:
+  	fcom 10
+  	j
+    fstsw ax
+    fwait
+    sahf
+  	fdiv 10
+
 	mov si, 0
 	mov output[si],'5'
 	mov ah, 09h
